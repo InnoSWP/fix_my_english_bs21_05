425a74e924b698e3bc5887b40878a952eebbd964
Blueberry13 <gee8way@gmail.com>
2022-06-06 22:43:50 +0300

    Initial

--- lib/analysis_page.dart
+++ lib/analysis_page.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart';
-import 'main.dart';
-import 'pdf_reader.dart';
-import 'main.dart';
-
-class _MyAnalysisPageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    // я хз. Я так и не заюзал еще стримы. Вот учусь...ок ща
-    //if (dataFromField.)
-    return Scaffold(
-      appBar: AppBar(
-        title: Image.asset(
-          'assets/logo.png',
-          height: 70,
-          alignment: Alignment.centerLeft,
-        ),
-      ),  // типо при разных нажатиях будут разные страницы открываться
-      body: Text(widget.text),
-      // тут надо вот смотреть и разные виды делать.
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'main.dart';
+// import 'pdf_reader.dart';
+// import 'main.dart';
+//
+// class _MyAnalysisPageState extends State<MyHomePage> {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // я хз. Я так и не заюзал еще стримы. Вот учусь...ок ща
+//     //if (dataFromField.)
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Image.asset(
+//           'assets/logo.png',
+//           height: 70,
+//           alignment: Alignment.centerLeft,
+//         ),
+//       ),  // типо при разных нажатиях будут разные страницы открываться
+//       body: Text(widget.text),
+//       // тут надо вот смотреть и разные виды делать.
+//     );
+//   }
+// }
