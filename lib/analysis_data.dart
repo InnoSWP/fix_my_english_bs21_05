c78b13bdd76b0cbd4b5a1d09088e98abc17bd85d
yazankb <99483577+yazankb@users.noreply.github.com>
2022-06-08 21:40:21 +0300

    Update analysis_data.dart
    1. creating a class that retains the API's returned analysis data
    2. Adding a function that transforms Json data to a list of AnalysisData

--- lib/analysis_data.dart
+++ lib/analysis_data.dart
@@ -1,7 +1,33 @@
-///Class that contains all data about some analysis of text (Sentences with issues, mistakes information)
-class AnalysisData {
-  //Text of analyzed text
-  String rawText;
+import 'dart:convert';
 
-  AnalysisData({required this.rawText});
+List<AnalysisData> analysisDataFromJson(String str) => List<AnalysisData>.from(json.decode(str).map((x) => AnalysisData.fromJson(x)));
+
+String analysisDataToJson(List<AnalysisData> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class AnalysisData {
+  AnalysisData({
+    this.match,
+    this.sentence,
+    this.label,
+    this.description,
+  });
+
+  String? match;
+  String? sentence;
+  String? label;
+  String? description;
+
+  factory AnalysisData.fromJson(Map<String, dynamic> json) => AnalysisData(
+    match: json["match"],
+    sentence: json["sentence"],
+    label: json["label"],
+    description: json["description"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "match": match,
+    "sentence": sentence,
+    "label": label,
+    "description": description,
+  };
 }
